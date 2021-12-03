ad63e6d76b18b2f3e4658cc030155b9345b81909
vaishnavipeyyeti <vaishnavipeyyeti@gmail.com>
2021-12-03 21:39:55 +0530

    80% done

--- assignment.R
+++ assignment.R
@@ -12,8 +12,21 @@ nrow(df2)
 #no. of 1 class passengers
 naval<-read.csv(file.choose(),header = F)
 head(naval)
+tail(naval)
 colnames(naval)<-c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","comp","turbine")
+d<-density(naval$comp)
+plot(d)
 pairs(head(naval))
+m1<-mean(naval$comp)
+m1
+m2<-mean(naval$turbine)
+m2
+med1<-median(naval$comp)
+med1
+med2<-median(naval$turbine)
+med2
+apply(naval,2,min)
+apply(naval,2,max)
 #
 library(MASS)
 bos<-MASS::Boston
