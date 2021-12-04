b245cf27070be036ff2ab8a52659dfe1970828b2
vaishnavipeyyeti <vaishnavipeyyeti@gmail.com>
2021-12-04 17:30:53 +0530

    naval scaling to be done

--- assignment.R
+++ assignment.R
@@ -1,22 +1,30 @@
 #naval data
-install.packages("titanic")
+#install.packages("titanic")
 library(titanic)
 df<-titanic::titanic_train
 head(df)
-df_subset<-subset(df,Sex=="male" & Survived==1)
+df_subset<-subset(df,Sex=="male" & Survived==1 & Age<=18)
 head(df_subset)
 nrow(df_subset)
 ###above is the no. of survived male child
 df2<-subset(df,Pclass==1)
 nrow(df2)
 #no. of 1 class passengers
+
+#naval dataset
 naval<-read.csv(file.choose(),header = F)
 head(naval)
 tail(naval)
 colnames(naval)<-c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","comp","turbine")
+#density fun
 d<-density(naval$comp)
 plot(d)
+#the curve is symmetric thus we can fit normal distribution
+
+#scatter plot matrix
 pairs(head(naval))
+
+#means and median
 m1<-mean(naval$comp)
 m1
 m2<-mean(naval$turbine)
@@ -25,11 +33,10 @@ med1<-median(naval$comp)
 med1
 med2<-median(naval$turbine)
 med2
+#apply min max scale
 apply(naval,2,min)
 apply(naval,2,max)
-#
+#Boston set
 library(MASS)
 bos<-MASS::Boston
 head(bos)
-min(bos$tax)
-
