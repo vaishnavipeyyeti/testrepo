17fd1a75901c6232f394aa9163859e54a0a1a619
vaishnavipeyyeti <vaishnavipeyyeti@gmail.com>
2021-12-05 19:28:08 +0530

    Done

--- proj1.R
+++ proj1.R
@@ -1,18 +1,34 @@
+#first we load the original data
+#i already imported it so i will just check with head
 head(winequality.red)
 str(winequality.red)
+#quality is int
+#duplicate the original set as wine
 wine<-winequality.red
 head(wine)
+summary(wine)
 
-wine$quality [wine$quality >5] <- 1
-wine$quality [wine$quality <= 5] <- 0
-#data.frame$column.name <- as.integer(data.frame$column.name)
+#classify data of quality binarily >5 and lessthan and = to
+wine$quality<-ifelse(wine$quality>5,1,0)
+#convert quality as factor
 wine$quality<-factor(wine$quality,levels = c(0,1),ordered = T)
+summary(wine)
 str(wine)
+#classification completed
+
+
+#starting regression(logistic)
 logisticmod<-glm(wine$quality~.,family = binomial(),data=wine)
 pred<-predict(logisticmod,type = "response")
+
+#create the input variables data set
 inputs<-wine[,1:11]
 head(inputs)
+
+#convert the 
 s=wine$quality==1
+
+#error value
 error<-sum((pred >= 0.5) != s)/nrow(inputs)
 error
 1-error
@@ -23,5 +39,7 @@ input_pred[input_pred>=0.5]<-1
 input_pred[input_pred!=1]<-0
 input_pred[is.na(input_pred)]<-0
 
-
+#compare our predicted value and the predicted value of the data source
+View(wine$quality)
+View(input_pred)
 write.table(input_pred, "predictions.csv",col.names = F,row.names=F,quote=FALSE)
