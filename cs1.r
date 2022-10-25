46fc1ccda65b15a51d2fe702a7e5a69876526b92
vaishnavipeyyeti <vaishnavipeyyeti@gmail.com>
2022-10-25 20:13:03 +0530

    8:12 25/10

--- cs1.r
+++ cs1.r
@@ -101,3 +101,135 @@ plot(waiting~eruptions,data=faithful,cex=.5,pch=19,col="blue")
 plot(waiting~eruptions, data=faithful, cex=.5, pch=19, col="blue", main="Old Faithful Eruptions",
      ylab="Wait time between eruptions", xlab="Duration of eruption")
 
+###################################################
+dbinom(0, 5, .5)
+#chap2.r
+s=1:20
+sample(s,100,replace = T)
+n=10
+p=0.3
+rbinom(100,n,p)
+dbinom(2,n,p)
+pbinom(2,n,p)
+qbinom(0.25,n,p)
+rgeom(10,0.3)
+dgeom(2,p)
+pgeom(2,p)
+qgeom(0.25,p)
+rnbinom(100,n,p)#p(y=2) type2 nb n==k
+dnbinom(2,n,p)# y is fail before k success
+pnbinom(2,n,p)
+qnbinom(0.25,n,p)
+rhyper(20,15,10,5)
+dhyper(2,10,15,5)
+#?dhyper
+lam=3.5
+rpois(10,lam)
+dpois(2,lam)
+ppois(2,lam)
+qpois(0.25,lam)
+runif(100,min=0,max=1)
+punif(1.2,min=0,max=1)
+dunif(1.3,min=0,max=1)
+gamma(3)
+rgamma(100,2,0.25)
+dgamma(3,2,0.25)
+pgamma(3,2,0.25)
+qgamma(0.25,3,0.25)
+rexp(100,1)
+dexp(2,1)
+pexp(2,1)
+qexp(0.25,1)
+rchisq(10,12)
+dchisq(2,12)
+#p
+#q
+beta(1,2)
+#rbeta(),q,p,d
+#norm
+#lnorm
+#f,t,
+
+##############################################
+dbinom(0:5, 5, .5)
+sum(dbinom(0:2, 5, .5)) # probability of 2 or fewer heads in 5 flips
+pbinom(2, 5, .5) # same as last line
+flip5 = replicate(10000, sum(sample(c("H","T"), 5, rep=T)=="H"))
+table(flip5) / 10000 # distribution (simulated) of count of heads in 5 flips
+table(rbinom(10000, 5, .5)) / 10000 # shorter version of previous 2 lines
+qbinom(seq(0,1,.2), 50, .2) # approx. 0/.2/.4/.6/.8/1-quantiles in Binom(50,.2) distribution
+binom.test(29, 200, .21) # inference on sample with 29 successes in 200 trials
+prop.test(29, 200, .21) # inference on same sample, using normal approx. to binomial
+
+######################################
+1 - pchisq(3.1309, 5) # gives P-value associated with X-squared stat 3.1309 when df=5
+pchisq(3.1309, df=5, lower.tail=F) # same as above
+qchisq(c(.001,.005,.01,.025,.05,.95,.975,.99,.995,.999), 2) # gives critical values like Table A
+qchisq(c(.999,.995,.99,.975,.95,.05,.025,.01,.005,.001), 2, lower.tail=F) # same as above
+observedCounts = c(35, 27, 33, 40, 47, 51)
+claimedProbabilities = c(.13, .13, .14, .16, .24, .20)
+chisq.test(observedCounts, p=claimedProbabilities) # goodness-of-fit test, assumes df = n-1
+############################################
+blood = read.csv("http://www.calvin.edu/~scofield/data/comma/blood.csv")
+t = table(blood$Rh, blood$type)
+t
+addmargins(t)
+addmargins(t,1)
+addmargins(t,2)
+
+########################################################################################################
+smoke = matrix(c(51,43,22,92,28,21,68,22,9),ncol=3,byrow=TRUE)
+colnames(smoke) = c("High","Low","Middle")
+
+rownames(smoke) = c("current","former","never")
+smoke = as.table(smoke)
+rowSums(smoke)
+colSums(smoke)
+prop.table(smoke)
+
+?prop.table()
+prop.table(smoke,1)#x/rowsum
+prop.table(smoke,2)#x/colsum
+barplot(smoke,legend=T,beside=T,main='Smoking Status by SES')
+
+################################################################################
+tblood = xtabs(~Rh + type, data=blood)
+tblood # contingency table for blood type and Rh factors
+chisq.test(tblood)
+fisher.test(tblood)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
